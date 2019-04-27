d37fd3cbcd6b5a656f18dc61a7fcb0a041431644
Ricard Delgado <ricard.delgado@gmail.com>
2019-04-27 23:15:48 +0200

    Before th enew round of features

--- features/__init__.py
+++ features/__init__.py
@@ -1,6 +1,4 @@
 from features.gen_predictor import gpi
-from features.gen_predictor import gpi1
-from features.gen_predictor import gpi2
 from features.lanl_features import create_all_features
 from features.lanl_features import add_trend_feature
 from features.lanl_features import sta_lta_ratio
--- utils/sampling.py
+++ utils/sampling.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def generate_segment_start_ids(sampling_method, segment_size, train):
+def generate_segment_start_ids(sampling_method, segment_size, train, quakes=None):
     if sampling_method == 'uniform':
         # With this approach we obtain 4194 segments
         num_segments_training = int(np.floor(train.shape[0] / segment_size))
@@ -15,12 +15,22 @@ def generate_segment_start_ids(sampling_method, segment_size, train):
         for i in range(num_segments_training):
             idx = i * segment_size
             # Detect if there is a discontinuity on the time_to_failure signal within the segment
-            max_jump = np.max(time_to_failure_jumps[idx:idx + segment_size])
-            if max_jump < 5:
-                segment_start_ids.append(idx)
-                num_good_segments_found += 1
+            if quakes is not None:
+                bad_quakes = quakes.loc[quakes['valid'] == False]
+                end_cond = (idx + segment_size) <= bad_quakes['end_idx']
+                start_cond = idx >= bad_quakes['start_idx']
+                cond = np.sum(start_cond * end_cond)
+                if cond==0:
+                    max_jump = np.max(time_to_failure_jumps[idx:idx + segment_size])
+                    if max_jump < 5:
+                        segment_start_ids.append(idx)
+                        num_good_segments_found += 1
             else:
-                print(f'Rejected candidate segment since max_jump={max_jump}')
+                max_jump = np.max(time_to_failure_jumps[idx:idx + segment_size])
+                if max_jump < 5:
+                    segment_start_ids.append(idx)
+                    num_good_segments_found += 1
+                    
         segment_start_ids.sort()
     elif sampling_method == 'random_no_jump':
         # With this approach we obtain 4194 segments
@@ -36,8 +46,6 @@ def generate_segment_start_ids(sampling_method, segment_size, train):
             if max_jump < 5:
                 segment_start_ids.append(idx)
                 num_good_segments_found += 1
-            else:
-                print(f'Rejected candidate segment since max_jump={max_jump}')
         segment_start_ids.sort()
     else:
         raise NameError('Method does not exist')
