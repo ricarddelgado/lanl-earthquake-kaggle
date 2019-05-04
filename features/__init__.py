cd10f9607d3ac4e494c67b4143fb606435c3dbab
Ricard Delgado <ricard.delgado@gmail.com>
2019-05-04 19:02:56 +0200

    Added new genetic predictor

--- features/__init__.py
+++ features/__init__.py
@@ -1,4 +1,5 @@
 from features.gen_predictor import gpi
+from features.gen_predictor_ii import gpi_new, gpii_new, gpiii_new
 from features.lanl_features import create_all_features
 from features.lanl_features import add_trend_feature
 from features.lanl_features import sta_lta_ratio
